a286e7957f09f97ee2ce4941c650b5d2dc6c35bf
ahmed badawi <ahmedbad063@gmail.com>
2025-06-30 10:38:05 +0300

    apps/client next initial commit

---
+++ apps/client/tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class", // Enables dark mode via class
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
