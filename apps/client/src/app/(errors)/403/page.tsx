6f212487320d20877118e84d6b652b3068b6f3ec
ahmed badawi <ahmedbad063@gmail.com>
2025-07-03 18:45:13 +0300

    finish custom errors pages first look

--- apps/client/src/app/(errors)/403/page.tsx
+++ apps/client/src/app/(errors)/403/page.tsx
@@ -3,16 +3,16 @@
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-export default function NotFoundError() {
+export default function ForbiddenError() {
   const router = useRouter();
   return (
     <div className="h-svh">
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
-        <h1 className="text-[7rem] font-bold leading-tight">404</h1>
-        <span className="font-medium">Oops! Page Not Found!</span>
+        <h1 className="text-[7rem] font-bold leading-tight">403</h1>
+        <span className="font-medium">Access Forbidden</span>
         <p className="text-center text-muted-foreground">
-          It seems like the page you&apos;re looking for <br />
-          does not exist or might have been removed.
+          You don&apos;t have necessary permission <br />
+          to view this resource.
         </p>
         <div className="mt-6 flex gap-4">
           <Button variant="outline" onClick={() => router.back()}>
