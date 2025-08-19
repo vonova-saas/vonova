4f2f5839b13871f95ad8c05ce874c4197b2a9503
ahmed badawi <ahmedbad063@gmail.com>
2025-08-19 14:58:26 +0300

    refactor course quizzes martial-library servcies and change to lms service

---
+++ services/lms/src/routes/quizzes/quiz.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { securityStack } from "../../middlewares/security";
+import * as QuizController from "../../controllers/quizzes/quiz.controller"
+import * as validationSchema from "../../validation/quizzes/quiz.validation";
+import { validateRequest } from "../../middlewares/validateRequest.middleware";
+import { isAuthenticated } from "../../middlewares/auth/isAuthenticated.middleware";
+import { isInstructorOrAdmin } from "../../middlewares/auth/isAuthorized.middleware";
+
+
+const quizRouter = Router();
+
+quizRouter.use(...securityStack)
+
+quizRouter.post("/addQuiz", isAuthenticated, isInstructorOrAdmin, validateRequest(validationSchema.createQuizSchema), QuizController.createQuiz);
+
+quizRouter.patch('/updateQuiz/:id', isAuthenticated, isInstructorOrAdmin, validateRequest(validationSchema.updateQuizSchema), QuizController.updateQuiz)
+
+
+
+export default quizRouter;
