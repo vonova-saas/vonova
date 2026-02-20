5f6e61b0105aa78b04a533d7ea6e5a5b8e51fa57
ahmed badawi <ahmedbad063@gmail.com>
2026-02-21 01:25:27 +0200

    solve app service problem

--- services/app/src/app.module.ts
+++ services/app/src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './common/config/configuration';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { SettingsModule } from './settings/settings.module';
 import { AccountModule } from './account/account.module';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
 import { BillingModule } from './billing/billing.module';
-import { SupportModule } from './support/support.module';
+import configuration from './common/config/configuration';
 import { FeedbackModule } from './feedback/feedback.module';
+import { SettingsModule } from './settings/settings.module';
+import { SupportModule } from './support/support.module';
 import { WaitlistModule } from './waitlist/waitlist.module';
+import { getMongoConfig } from './common/config/mongo.config';
 
 @Module({
   imports: [
@@ -18,7 +19,12 @@ import { WaitlistModule } from './waitlist/waitlist.module';
       envFilePath: '.env',
       load: [configuration],
     }),
-    MongooseModule.forRoot(configuration().MONGO_URI_LOCAL!),
+    // MongooseModule.forRoot(configuration().MONGO_URI_LOCAL!),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getMongoConfig,
+      inject: [],
+    }),
     //? App Models
     AuthModule,
     WaitlistModule,
