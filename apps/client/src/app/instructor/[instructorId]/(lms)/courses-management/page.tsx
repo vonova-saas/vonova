1995ea24b823410a9e6f6a6b8c7d1accff546041
ahmed badawi <ahmedbad063@gmail.com>
2026-04-20 17:35:49 +0200

    refatcor client dashboard and delete feedback feature

--- apps/client/src/app/instructor/[instructorId]/(lms)/courses-management/page.tsx
+++ apps/client/src/app/instructor/[instructorId]/(lms)/courses-management/page.tsx
@@ -1,7 +1,57 @@
-import DashboardComingSoon from "@/components/global/dashboard-coming-soon";
+import { adminGetCourses } from "@/components/instructor/lms/courses-management/data/admin-get-courses";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { AdminCourseCard, AdminCourseCardSkeleton } from "@/components/instructor/lms/courses-management/admin-course-card";
+import EmptyState from "@/components/instructor/lms/courses-management/general/empty-state";
+import { Suspense } from "react";
 
 export default function CoursesManagementPage() {
   return (
-    <DashboardComingSoon pageName="Course Management" />
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+
+        <Link className={buttonVariants()} href={`courses-management/create`}>
+          Create Course
+        </Link>
+      </div>
+
+      <Suspense fallback={<AdminCourseCardSkeletonLayout />}>
+        <RenderCourses />
+      </Suspense>
+    </>
   );
+}
+
+async function RenderCourses() {
+  const data = await adminGetCourses();
+
+  return (
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="No Courses found"
+          description="Create a new course to get started"
+          buttonText="Create Course"
+          href={`courses-management/create`}
+        />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+          {data.map((course) => (
+            <AdminCourseCard key={course.id} data={course} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+function AdminCourseCardSkeletonLayout() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-7">
+      {Array.from({ length: 4 }).map((_, index) => (
+        <AdminCourseCardSkeleton key={index} />
+      ))}
+    </div>
+  )
 }
--- apps/client/src/components/student/main/sidebar-nav-config.ts
+++ apps/client/src/components/student/main/sidebar-nav-config.ts
@@ -11,7 +11,6 @@ import {
   // TvMinimalPlay,
   Zap,
   LifeBuoy,
-  Send,
   User,
   BrainCog,
 } from "lucide-react";
@@ -93,11 +92,6 @@ export const sidebarNavData = {
       title: "Support",
       url: "/student/:studentId/support",
       icon: LifeBuoy,
-    },
-    {
-      title: "Feedback",
-      url: "/student/:studentId/feedback",
-      icon: Send,
     },
   ],
 };
